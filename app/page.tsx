be42467a426176f85e620e15eb3c3aa2b7108fe2
atharv-av <atharvv333@gmail.com>
2024-03-06 20:30:09 +0530

    completed sentiment section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "../components/Navbar";
 import MobileNavigation from "../components/MobileNavigation";
 import MainContent from "../components/MainContent";
-import TradingWidget from "../components/TradingWidget";
 
 export default function Home() {
   return (
--- components/Sentiment.tsx
+++ components/Sentiment.tsx
@@ -18,11 +18,27 @@ interface event {
   description: string;
 }
 
-const keyEvents: event[] = [
+interface estimate {
+  title: string;
+  color: string;
+  value: number;
+}
+
+const analystEstimates: estimate[] = [
   {
-    icon: "",
-    title: "",
-    description: "",
+    title: "Buy",
+    color: "bg-green-500",
+    value: 76,
+  },
+  {
+    title: "Hold",
+    color: "bg-gray-500",
+    value: 8,
+  },
+  {
+    title: "Sell",
+    color: "bg-red-500",
+    value: 16,
   },
 ];
 
@@ -30,54 +46,75 @@ const Sentiment: React.FC = () => {
   return (
     <div className="w-full mt-8 p-6 bg-white rounded-lg flex flex-col gap-6">
       <p className="font-semibold text-2xl">Sentiment</p>
-
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-3">
           <div className="flex items-center gap-2">
             <p className="font-semibold text-slate-600 text-lg">Key Events</p>
             <Info className="text-white bg-slate-500 rounded-full" size={20} />
           </div>
-       <div className="px-8">
-       <Carousel className="w-full max-w-2xl">
-            <CarouselContent className="-ml-1">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem
-                  key={index}
-                  className="pl-1 md:basis-1/2 lg:basis-1/2"
-                >
-                  <div className="p-1">
-                    <Card className="">
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-2xl font-semibold">
-                          {index + 1}
-                        </span>
-                      </CardContent>
-                    </Card>
+          <div className="px-8">
+            <Carousel
+              opts={{
+                align: "start",
+              }}
+              className="w-full max-w-2xl"
+            >
+              <CarouselContent className="">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <div className="mr-10">
+                    <CarouselItem
+                      key={index}
+                      className="md:basis-1/2 lg:basis-1/2 basis-1"
+                    >
+                      <div className="p-1">
+                        <div className="h-52 w-96 rounded-lg bg-blue-200 flex flex-col gap-4 p-3">
+                          <div className="flex flex-row gap-2">
+                            <div className="text-white bg-blue-500 rounded-full p-3 h-fit">
+                              <BookText />
+                            </div>
+                            <p className="font-semibold text-base">
+                              Lorem ipsum dolor sit, amet consectetur
+                              adipisicing elit.
+                            </p>
+                          </div>
+                          <div className="font-light text-base ml-14">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, facilis quo! Cumque adipisci fuga obcaecati magnam, minus ad molestias.
+                          </div>
+                        </div>
+                      </div>
+                    </CarouselItem>
                   </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselNext />
-            <CarouselPrevious />
-          </Carousel>
-       </div>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col">
-            <div className="flex items-center gap-2">
-              <p className="font-semibold text-slate-600 text-lg">
-                Analyst Estimates
-              </p>
-              <Info
-                className="text-white bg-slate-500 rounded-full"
-                size={20}
-              />
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-slate-600 text-lg">
+              Analyst Estimates
+            </p>
+            <Info className="text-white bg-slate-500 rounded-full" size={20} />
+          </div>
+          <div className="flex items-center gap-8">
+            <div className="text-xl font-semibold text-green-600 bg-green-100 rounded-full p-8">
+              76%
             </div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-            recusandae exercitationem consequuntur est numquam dolorum inventore
-            dignissimos, dolores quidem maxime blanditiis porro a! Totam cumque
-            tenetur, minima ea officiis magnam!
+            <div className="flex flex-col gap-2 w-1/2">
+              {analystEstimates.map((estimate) => (
+                <div className="w-full flex gap-4 items-center justify-start">
+                  <p className="text-sm w-6 text-slate-600">{estimate.title}</p>
+                  <div
+                    style={{ width: `${estimate.value}%` }}
+                    className={`h-2 ${estimate.color} rounded-full`}
+                  ></div>
+                  <p className="text-sm text-slate-600">{estimate.value}%</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
